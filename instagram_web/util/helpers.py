a58ea5bd892df835d50f103b2b3ad7938688e37f
Lin Wei Tatt <weitatt@Lins-MacBook-Pro.local>
2019-07-25 11:45:08 +0800

    added oAuth register

--- instagram_web/util/helpers.py
+++ instagram_web/util/helpers.py
@@ -1,6 +1,8 @@
 import boto3
 import botocore
 import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 
 S3_BUCKET = os.getenv('S3_BUCKET')
 S3_KEY = os.getenv('S3_ACCESS_KEY')
@@ -28,3 +30,19 @@ def upload_file_to_s3(file, bucket_name, acl="public-read"):
         print("Something happened: ", e)
         return e
     return file.filename
+
+
+def send_email():
+    message = Mail(
+        from_email='from_email@example.com',
+        to_emails='to@example.com',
+        subject='Sending with Twilio SendGrid is Fun',
+        html_content='<strong>and easy to do anywhere, even with Python</strong>')
+    try:
+        sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+        response = sg.send(message)
+        print(response.status_code)
+        print(response.body)
+        print(response.headers)
+    except Exception as e:
+        print(e.message)
